d503ab5184106c61324ee0e200bc05c405b8c3ad
Harsh <doingonlygoodthings@gmail.com>
2025-09-28 14:56:04 +0000

    5th roll no digit

--- models/Student.js
+++ models/Student.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  roll_no: { type: String, unique: true, required: true },
+  roll_no: { type: String, unique: true, required: true, match: [/^\d{1,6}$/, "Roll number must be only digits and max 6 characters"]},
   password: { type: String, required: true }
 });
 
